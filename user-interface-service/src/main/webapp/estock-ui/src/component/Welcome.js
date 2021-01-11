ae80b2de9ffe59fea78dc0897b987b355f854f01
Vinayak Chaturvedi <vinayak.chaturvedi96@gmail.com>
2021-01-12 02:43:01 +0530

    Adding Welcome page

--- user-interface-service/src/main/webapp/estock-ui/src/component/Welcome.js
+++ user-interface-service/src/main/webapp/estock-ui/src/component/Welcome.js
@@ -2,17 +2,30 @@ import React, {Component} from "react";
 
 class Welcome extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            img: "./background1.jpg"
-        }
-    }
-
     render() {
         return (
             <div>
-                <img src={this.state.img}/>
+                <nav>
+                    <input type="checkbox" id="check"/>
+                    <label for="check" className="checkBtn">
+                        <i className="fas fa-bars"/>
+                    </label>
+                    <label className="logo">EStock</label>
+                    <ul>
+                        <li><a href="https://www.w3schools.com">Home</a></li>
+                        <li><a href="https://www.w3schools.com">About</a></li>
+                        <li><a href="https://www.w3schools.com">Services</a></li>
+                        <li><a href="https://www.w3schools.com">Contact</a></li>
+                        <li><a href="https://www.w3schools.com">Sign in</a></li>
+                    </ul>
+                </nav>
+                <div className="content">
+                    <h2>A New Way to Invest</h2>
+                    <p>EStock is the best platform to help you analyse the latest stock trends
+                        and decide the stock through which you can grow your wealth.</p>
+
+                    <button className="createAccount">Create Account ></button>
+                </div>
             </div>
         );
     }
