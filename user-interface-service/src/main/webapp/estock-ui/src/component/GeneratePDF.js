d94f63698076c02295c82cfb9d24707b843acc2a
shubhi2206 <maheshwarishubhi22@gmail.com>
2021-04-07 09:25:53 +0530

    PDF Dialogue Box Updated!

--- user-interface-service/src/main/webapp/estock-ui/src/component/GeneratePDF.js
+++ user-interface-service/src/main/webapp/estock-ui/src/component/GeneratePDF.js
@@ -5,13 +5,65 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import {Document, Link, Page, PDFDownloadLink, StyleSheet, Text, View} from "@react-pdf/renderer";
+
+const styles = StyleSheet.create({
+    title: {
+        margin: 20,
+        fontSize: 25,
+        textAlign: 'center',
+        backgroundColor: '#e4e4e4',
+        textTransform: 'uppercase'
+    },
+    body: {
+        flexGrow: 1,
+    },
+    row: {
+        flexGrow: 1,
+        flexDirection: 'row',
+    },
+    block: {
+        flexGrow: 1,
+    },
+    text: {
+        width: '60%',
+        margin: 10,
+        textAlign: 'justify',
+    },
+    textItalic: {
+        width: '60%',
+        margin: 10
+    },
+    fill1: {
+        width: '40%',
+        backgroundColor: '#e14427',
+    },
+    fill2: {
+        flexGrow: 2,
+        backgroundColor: '#e6672d',
+    },
+    fill3: {
+        flexGrow: 2,
+        backgroundColor: '#e78632',
+    },
+    fill4: {
+        flexGrow: 2,
+        backgroundColor: '#e29e37',
+    },
+});
 
 class GeneratePDF extends React.Component{
     constructor(props) {
         super(props);
         this.state={
             open : false,
-            setOpen  :false
+            setOpen  :false,
+            stockName : this.props.location.stockName,
+            tradingAccount  :this.props.location.tradingAccount,
+            price : this.props.location.price,
+            tradingDate : this.props.location.tradingDate,
+            quantity : this.props.location.quantity,
+            netAmount  :this.props.location.netAmount
         }
 
         this.handleClose = this.handleClose.bind(this)
@@ -23,9 +75,35 @@ class GeneratePDF extends React.Component{
         })
     }
 
+    MyDocument(stockName, tradingAccount, price, tradingDate, quantity, netAmount){
+        return <Document>
+            <Page size="A4">
+                <Link
+                    style={styles.title}
+                    src="https://es.wikipedia.org/wiki/Lorem_ipsum"
+                >
+                    Lorem Ipsum
+                </Link>
+                <View style={styles.body}>
+                    <View style={styles.row}>
+                        <Text style={styles.text}>
+                            Lorem ipsum dolor sit amet, consectetur, {stockName}, {price} {tradingAccount}, {tradingDate},
+                            {quantity}, {netAmount}
+                        </Text>
+                        <View style={styles.fill1} />
+                    </View>
+                </View>
+            </Page>
+        </Document>
+    }
+
     render() {
         return (
+
             <div>
+                <div>
+                    {this.state.price}, {this.state.tradingDate}
+                </div>
                 <Dialog
                     open={true}
                     keepMounted
@@ -36,13 +114,14 @@ class GeneratePDF extends React.Component{
                     <DialogTitle id="alert-dialog-slide-title">{"Stocks have been purchased!"}</DialogTitle>
                     <DialogContent>
                         <DialogContentText id="alert-dialog-slide-description">
-                            The puchase has been completed. Kindly click on the link to download the invoice!
+                            The purchase has been completed. Kindly click on the link to download the invoice!
                         </DialogContentText>
                     </DialogContent>
                     <DialogActions>
-                        <Button onClick={this.handleClose} color="primary">
-                            Disagree
-                        </Button>
+                        <PDFDownloadLink document={this.MyDocument(this.state.stockName,
+                            this.state.tradingAccount, this.state.price, this.state.tradingDate, this.state.quantity, this.state.netAmount)} fileName="somename.pdf">
+                            {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download now!')}
+                        </PDFDownloadLink>
                         <Button onClick={this.handleClose} color="primary">
                             Agree
                         </Button>
